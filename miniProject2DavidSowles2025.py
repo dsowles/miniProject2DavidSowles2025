3e9edb586c370abd8f0c460ffb38d581b5f4393a
dsowles <d_sowles@mail.fhsu.edu>
2025-09-21 16:41:50 -0500

    Project reset. Check to see if all modules have been installed and loaded correctly.

--- miniProject2DavidSowles2025.py
+++ miniProject2DavidSowles2025.py
@@ -1,82 +1,21 @@
 
-import matplotlib.pyplot as plt
-import pandas as pd
 import numpy as np
-import faker
 import os
+import pandas as pd
+import matplotlib.pyplot as plt
+import faker
+
 
 
 
 print("Hello World!")
 
-'''
-fake = faker.Faker()
+print('\n')
 
-
-print(fake.state())
-print(fake.pyfloat(max_value=5000, min_value=0, right_digits=2))
-
-data1 = [
-             { "first_name": fake.first_name(),
-               "last_name":  fake.last_name(),
-               "address": fake.address(),
-               "email": fake.email(),
-               "age": np.random.randint(18, 80)
-             }
-             for i in range(1000)
-         ]
-
-plot1 = pd.DataFrame(data = data1)
-plot1.plot()
-
-plt.show()
-
-
-
-
-data2 = [
-             { "first_name": fake.first_name(),
-               "last_name":  fake.last_name(),
-               "state": fake.state(),
-               "email": fake.email(),
-               "age": np.random.randint(18, 80),
-               "traffic tickets" : np.random.randint(0,12)
-             }
-             for i in range(1000)
-         ]
-
-plot2 = pd.DataFrame(data = data2)
-
-plot2["age"].plot()
-plt.show()
-
-
-
-
-
-data3 = { "Name" : [] , "State" : [], "Cash" : [] }
-
-for i in range(10) :
-    data3["Name"].append(fake.name())
-    data3["State"].append(fake.state())
-    data3["Cash"].append(np.random.randint(0, 20000))
-
-df = pd.DataFrame(data = data3)
-
-print(list(df["State"]))
-print(list(df["State"].keys()))
-
-plt.plot(list(df["State"]), list(df["Cash"]))
-plt.show()
-
-print("Stuff")
-
-
-
-print(df["State"].keys())
-plot3.set_xticks(df["State"].keys, df["State"].values)
-
-plt.show()
-
-'''
+# Check to see if packages are all loaded
+print(f'Is matplotlib.pyplot loaded?: {plt}')
+print(f'Is pandas loaded? {pd}')
+print(f'Is numpy loaded?: {np}')
+print(f'Is faker loaded?: {faker}')
+print(f'Is OS module loaded?: {os}')
 
