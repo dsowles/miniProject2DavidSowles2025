1cf08d16fbac92b7c465c138ceb8c98b7452f661
dsowles <d_sowles@mail.fhsu.edu>
2025-09-21 21:33:51 -0500

    Added beginning comment to app file.

--- miniProject2DavidSowles2025.py
+++ miniProject2DavidSowles2025.py
@@ -1,3 +1,19 @@
+'''
+INF601 - Programming in Python
+Assignment miniProject2
+
+# Filename:         miniProject2DavidSowles2025.py
+# Author:           David A. Sowles
+# Creation Date:    09/21/2025
+# Last Updated:     09/21/2025
+
+I,     David Sowles    , affirm that the work submitted for this assignment is entirely my own. 
+I have not engaged in any form of academic dishonesty, including but not limited to cheating, plagiarism, or the use of unauthorized materials. 
+I have neither provided nor received unauthorized assistance and have accurately cited all sources in adherence to academic standards. 
+I understand that failing to comply with this integrity statement may result in consequences, including disciplinary actions as determined 
+by my course instructor and outlined in institutional policies. By signing this statement, I acknowledge my commitment to upholding the principles of academic integrity.
+'''
+
 
 import numpy as np
 import os
